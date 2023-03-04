1062a3be3eb39cde967d6b1ed9f1dd1e043bd006
Akasaka Hajime <shamshel0207@gmail.com>
2023-03-04 10:52:53 +0900

    トップページの作成(ヘッダーとTOP画像)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require popper
+//= require bootstrap
+//= require_tree .
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap';
+import "../stylesheets/application.scss";
 
 Rails.start()
 Turbolinks.start()
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,13 @@ module SukiyakiEncyclopedia
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
